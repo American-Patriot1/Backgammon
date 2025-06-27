8be69a14f29500a90e2beca963abd7433248e7a2
Nathaniel <nathanielhavens5@gmail.com>
2025-06-27 11:40:12 -0500

    created the board

--- backgammon.js
+++ backgammon.js
@@ -1,4 +1,4 @@
-const canvas = document.getElementById('backgammon');
+const canvas = document.getElementById('backgammonCanvas');
 const ctx = canvas.getContext('2d');
 
 
@@ -6,8 +6,13 @@ const ctx = canvas.getContext('2d');
 const canvasWidth = canvas.width;
 const canvasHeight = canvas.height;
 
+
+const squareSize = 20;
+const numRows = canvasHeight / squareSize;
+const numCols = canvasWidth / squareSize;
+
 function drawBoard() {
-    tx.fillRect(20, 10, 200, 100);
+    
     // for (let row = 0; row < numRows; row++) {
     //     for (let col = 0; col < numCols; col++) {
     //         const color = (row + col) % 2 === 0 ? 'DarkGrey' : 'Grey';
@@ -15,12 +20,36 @@ function drawBoard() {
     //         ctx.fillRect(col * squareSize, row * squareSize, squareSize, squareSize);
     //     }
     // }
+    for (let amt = 0; amt<=25; amt++){
+        let spikeTopPosition = 120;
+        let spikeBottomPosition = 0;
+        let reset=0
+        if((amt>12)){
+            spikeTopPosition = 180;
+            spikeBottomPosition = 300;
+            reset=325
+        }
+        ctx.beginPath();
+        let num=amt%2;
+        console.log(num);
+        if(amt%2==1){
+            ctx.fillStyle = "#020215";
+        }else if(amt%2==0){
+            ctx.fillStyle = "white";
+        }
+        ctx.moveTo(40+(amt*25)-reset, spikeBottomPosition);
+        ctx.lineTo(50+(amt*25)-reset, spikeTopPosition);
+        ctx.lineTo(60+(amt*25)-reset, spikeBottomPosition);
+        ctx.lineTo(40+(amt*25)-reset, spikeBottomPosition);
+        ctx.fill();
+        ctx.fillStyle = "#020215";
+        ctx.fillRect(190, 0, 20, 300);
+        ctx.fillRect(20, 0, 15, 300);
+        ctx.fillRect(365, 0, 15, 300);
+        ctx.fillRect(0, 100, 20, 100);
+        ctx.fillRect(380, 100, 20, 100);
+    }
 }
-
-
-
-
-
 
 function gameLoop(currentTime) {
     const deltaTime = currentTime - lastTime;
